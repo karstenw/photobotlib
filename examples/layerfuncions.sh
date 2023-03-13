3755240a16edd9f24291598224a56df6064cc8b1
Karsten Wolf <karstenwo@web.de>
2023-03-13 23:49:33 +0100

    Added py3 versions.

--- examples/layerfuncions.sh
+++ examples/layerfuncions.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-python2 Layer_colorize.py
-python2 explore_gradients 1.py
 
+python2 Layer_colorize.py
+python2 "explore_gradients 1.py"
 python2 Layer_filter_boxblur.py
 python2 Layer_filter_contour.py
 python2 Layer_filter_edge_enhance_more.py
@@ -27,3 +27,31 @@ python2 Layer_function_select.py
 python2 Layer_function_solarize.py
 python2 Layer_function_subtract_modulo.py
 python2 Layer_function_subtract.py
+
+python3 Layer_colorize.py
+python3 "explore_gradients 1.py"
+python3 Layer_filter_boxblur.py
+python3 Layer_filter_contour.py
+python3 Layer_filter_edge_enhance_more.py
+python3 Layer_filter_edge_enhance.py
+python3 Layer_filter_emboss.py
+python3 Layer_filter_find_edges.py
+python3 Layer_function_add_modulo.py
+python3 Layer_function_add.py
+python3 Layer_function_autocontrast.py
+python3 Layer_function_brightness.py
+python3 Layer_function_color.py
+python3 Layer_function_contrast.py
+python3 Layer_function_difference.py
+python3 Layer_function_flip.py
+python3 Layer_function_hue.py
+python3 Layer_function_mask.py
+python3 Layer_function_multiply.py
+python3 Layer_function_opacity.py
+python3 Layer_function_overlay.py
+python3 Layer_function_posterize.py
+python3 Layer_function_screen.py
+python3 Layer_function_select.py
+python3 Layer_function_solarize.py
+python3 Layer_function_subtract_modulo.py
+python3 Layer_function_subtract.py
