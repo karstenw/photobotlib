e89437033b0b91bd1834a5d053339f3e3f4ab7ad
Karsten Wolf <karstenwo@web.de>
2023-09-05 15:07:38 +0200

    Update setup.py
    silenced some requirements due to security warning for pillow <8.3.2

--- setup.py
+++ setup.py
@@ -4,7 +4,9 @@ setup(name='photobot',
       version='1.2',
       packages=['photobot'],
       install_requires=[
-            'Pillow == 6.2.2; python_version < "3.0.0"',
-            'Pillow >= 7.1.0; python_version >= "3.5.0"']
+            # silenced due to security warning for pillow <8.3.2; should still run with python2
+            # 'Pillow == 6.2.2; python_version < "3.0.0"',
+            # 'Pillow >= 7.1.0; python_version >= "3.5.0"']
+            'Pillow >= 8.3.2; python_version >= "3.5.0"']
       )
 
