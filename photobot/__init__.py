094e44f960064398de06fbccb201bb55c5e7b9e7
Karsten Wolf <karstenwo@web.de>
2026-03-24 11:41:01 +0100

    explodeRectangles() - @Work

--- photobot/__init__.py
+++ photobot/__init__.py
@@ -1984,7 +1984,7 @@ with Image.open("hopper.jpg") as im:
 def calculateRectangles(width, height):
     """Calculate several rectangles for the given size.
     
-    Returns a namedtuple( innerSquare outerSquare upper lower left right quads niner outerNiner )
+    Returns a namedtuple( innerSquare outerSquare upper lower left right quads niner outerNiner threeRows threeColumns )
         
         A rectangle in this context means a tuple with ( x,y,w,h ) - the rectangle class is not yet integrated
         
@@ -2100,6 +2100,89 @@ def calculateRectangles(width, height):
     return result
 
 
+def explodeRectangles( rectangles, deltax=10, deltay=10 ):
+    
+    # innerSquare outerSquare upper lower left right quads niner outerNiner threeRows threeColumns
+    upper = rectangles.upper 
+    if upper is not None:
+        upper = ( upper[0], upper[1] - deltay, upper[2], upper[3] )
+    
+    lower = rectangles.lower
+    if lower is not None:
+        lower = ( lower[0], lower[1] + deltay, lower[2], lower[3] )
+    
+    left = rectangles.left
+    if left is not None:
+        left = ( left[0] - deltax, left[1], left[2], left[3] )
+    
+    right = rectangles.right
+    if right is not None:
+        right = ( right[0] + deltax, right[1], right[2], right[3] )
+    
+    # QUADS
+    dx2 = deltax / 2
+    dy2 = deltay / 2
+    q1, q2, q3, q4 = rectangles.quads
+    quads = (
+        ( q1[0] - dx2, q1[1] - dy2, q1[2], q1[3] ),
+        ( q2[0] + dx2, q2[1] - dy2, q2[2], q2[3] ),
+        
+        ( q3[0] - dx2, q3[1] + dy2, q3[2], q3[3] ),
+        ( q4[0] + dx2, q4[1] + dy2, q4[2], q4[3] )
+    )
+    
+    # NINER
+    dx3 = deltax / 2
+    dy3 = deltay / 2
+    q1, q2, q3, q4, q5, q6, q7, q8, q9 = rectangles.niner
+    niner = (
+        ( q1[0] - dx3, q1[1] - dy3, q1[2], q1[3] ),
+        ( q2[0]      , q2[1] - dy3, q2[2], q2[3] ),
+        ( q3[0] + dx3, q3[1] - dy3, q3[2], q3[3] ),
+        
+        ( q4[0] - dx3, q4[1]      , q4[2], q4[3] ),
+        ( q5[0]      , q5[1]      , q5[2], q5[3] ),
+        ( q6[0] + dx3, q6[1]      , q6[2], q6[3] ),
+        
+        ( q7[0] - dx3, q7[1] + dy3, q7[2], q9[3] ),
+        ( q8[0]      , q8[1] + dy3, q8[2], q8[3] ),
+        ( q9[0] + dx3, q9[1] + dy3, q9[2], q7[3] )
+    )
+    
+    # OUTERNINER
+    q1, q2, q3, q4, q5, q6, q7, q8, q9 = rectangles.outerNiner
+    outerNiner = (
+        ( q1[0] - dx3, q1[1] - dy3, q1[2], q1[3] ),
+        ( q2[0]      , q2[1] - dy3, q2[2], q2[3] ),
+        ( q3[0] + dx3, q3[1] - dy3, q3[2], q3[3] ),
+        
+        ( q4[0] - dx3, q4[1]      , q4[2], q4[3] ),
+        ( q5[0]      , q5[1]      , q5[2], q5[3] ),
+        ( q6[0] + dx3, q6[1]      , q6[2], q6[3] ),
+        
+        ( q7[0] - dx3, q7[1] + dy3, q7[2], q9[3] ),
+        ( q8[0]      , q8[1] + dy3, q8[2], q8[3] ),
+        ( q9[0] + dx3, q9[1] + dy3, q9[2], q7[3] )
+    )
+    
+    # THREEROWS
+    q1, q2, q3 = rectangles.threeRows
+    threeRows = (
+        ( q1[0]      , q1[1] - dy3, q1[2], q1[3] ),
+        ( q2[0]      , q2[1]      , q2[2], q2[3] ),
+        ( q3[0]      , q3[1] + dy3, q3[2], q3[3] )
+    )
+    
+    # THREECOLUMNS
+    q1, q2, q3 = rectangles.threeColumns
+    threeColumns = (
+        ( q1[0] - dx3, q1[1]      , q1[2], q1[3] ),
+        ( q2[0]      , q2[1]      , q2[2], q2[3] ),
+        ( q3[0] + dx3, q3[1]      , q3[2], q3[3] )
+    )
+    
+    result = Rectangles( rectangles.innerSquare, rectangles.outerSquare, upper, lower, left, right, quads, niner, outerNiner, threeRows, threeColumns )
+    return result
 def aspectRatio(size, maxsize, height=False, width=False, assize=False):
     """Resize image with size=(w,h) to maxsize in max(width, height).
     use height == maxsize if height==True
