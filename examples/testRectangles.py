c92c95d8bf9f610339815f64d4f43cae0e2de3cf
Karsten Wolf <karstenwo@web.de>
2026-08-04 12:20:15 +0200

    More rectangely work

--- examples/testRectangles.py
+++ examples/testRectangles.py
@@ -1,8 +1,19 @@
 
 
 """Create all Rectangle combinations and create an image."""
-
-
+import sys, random
 import photobot
-photobot.testRectangles()
+import imagewells
+loadImageWell = imagewells.loadImageWell
+imagewell = loadImageWell(   bgsize=(2048, 1440),
+                             minsize=(256,256),
+                             pathonly=True,
+                             ignoreDotFolders=True,
+                             ignoreFolderNames=('+offline', '+OFFLINE'))
+
+# tiles are images >256x256 and <=WIDTH, HEIGHT
+tiles = imagewell['tiles']
+
+path = random.choice( tiles )
+photobot.testRectangles( path, 30)
 
